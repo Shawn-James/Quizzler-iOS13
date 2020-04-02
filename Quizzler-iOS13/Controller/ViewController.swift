01534f9b0922cdfaa5172dcf507d0ba3fbcd61fa
Shawn-James <shawn.james@me.com>
2020-04-02 16:14:45 -0400

    first commit

---
+++ Quizzler-iOS13/Controller/ViewController.swift
@@ -0,0 +1,59 @@
+//
+//  ViewController.swift
+//  Quizzler-iOS13
+//
+//  Created by Angela Yu on 12/07/2019.
+//  Copyright © 2019 The App Brewery. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    
+    @IBOutlet weak var questionLabel: UILabel!
+    @IBOutlet weak var progressBar: UIProgressView!
+    @IBOutlet weak var trueButton: UIButton!
+    @IBOutlet weak var falseButton: UIButton!
+    @IBOutlet weak var scoreLabel: UILabel!
+    
+    
+    var quizBrain = QuizBrain()
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        updateUI()
+    }
+    
+    
+    @IBAction func answerButtonTapped(_ sender: UIButton) {
+        let userAnswer = sender.currentTitle!
+        let userGotItRight = quizBrain.checkAnswer(userAnswer)
+        
+        if userGotItRight {
+            sender.backgroundColor = UIColor.systemGreen
+        } else {
+            sender.backgroundColor = UIColor.systemRed
+        }
+                
+        DispatchQueue.main.asyncAfter(deadline: .now() + 1.0) {
+            self.trueButton.backgroundColor = UIColor.clear
+            self.falseButton.backgroundColor = UIColor.clear
+            self.updateUI()
+        }
+        
+        quizBrain.nextQuestion()
+        
+    }
+    
+    
+    func updateUI() {
+        questionLabel.text = quizBrain.getQuestionText()
+        progressBar.progress = quizBrain.getProgress()
+        scoreLabel.text = "Score: \(quizBrain.getScore())"
+    }
+    
+    
+}
---
+++ Quizzler-iOS13/Model/QuizQuestion.swift
@@ -0,0 +1,14 @@
+//
+//  QuizQuestion.swift
+//  Quizzler-iOS13
+//
+//  Created by Shawn James on 4/1/20.
+//  Copyright © 2020 The App Brewery. All rights reserved.
+//
+
+import Foundation
+
+struct QuizQuestion {
+    let question: String
+    let answer: String
+}
